c0595db3e8829b9ddcba3389e69edc2e04bac9b8
Marcelo Alves <marcelo.asf92@gmail.com>
2022-11-20 07:20:03 -0300

    fix: protect routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,26 @@
-import { Routes as RoutesDOM, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  Routes as RoutesDOM,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+import { useLoginContext } from "../contexts";
 import { Home, Login, Dashboard } from "../pages";
 
 export const Routes = () => {
+  let navigate = useNavigate();
+
+  const { token } = useLoginContext();
+
+  useEffect(() => {
+    if (token) {
+      return navigate("/dashboard");
+    } else {
+      return navigate("/");
+    }
+  }, [token]);
+
   return (
     <RoutesDOM>
       <Route path="/" element={<Home />} />
